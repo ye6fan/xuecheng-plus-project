0426ff25c205bd4a544193c501307fb3c43b1a1a
ye6fan <2083923479@qq.com>
2023-09-28 19:36:13 +0800

    9-28=19-36

---
+++ project-xczx2-portal-vue-ts/src/components/portal-footer/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-7">
+          <div>
+            <!--<h1 style="display: inline-block">叶凡网</h1>-->
+            <img src="@/assets/img/logoIco.png" alt />
+          </div>
+          <div>叶凡课堂是在线学习平台</div>
+          <div>© 2023年保留所有权利。-豫ICP备2023006358号</div>
+        </div>
+        <div class="col-md-5 row">
+          <dl class="col-md-4">
+            <dt>合作伙伴</dt>
+            <dd>阿李巴巴</dd>
+            <dd>腾迅</dd>
+            <dd>字节调动</dd>
+            <dd>哔里哔里</dd>
+            <dd>网宜</dd>
+            <dd>米哈鼬</dd>
+          </dl>
+        </div>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "PortalFooter"
+})
+export default class extends Vue {}
+</script>
+
+<style lang="scss" scoped>
+@import "index";
+</style>
---
+++ project-xczx2-portal-vue-ts/src/module-organization/components/nav-bar.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="nav-bar">
+        <el-tabs class="el-tabs" v-model="activeName" type="border-card" @tab-click="handleClick">
+            <el-tab-pane label="网站首页">
+                <a href="http://localhost:7777">叶凡课堂首页</a>
+            </el-tab-pane>
+            <el-tab-pane label="教务教学" name="second">
+                <router-link to="/organization/course-list">课程管理</router-link>
+                <div class="divider"></div>
+                <router-link to="/organization/media-list">媒资管理</router-link>
+                <div class="divider"></div>
+            </el-tab-pane>
+            <el-tab-pane label="问题">
+                <a href="http://localhost:7777">虽然点击发布会报404，但是后台正常运行，不影响工作，只是前端参数的原因</a>
+            </el-tab-pane>
+        </el-tabs>
+    </div>
+</template>
+
+<style lang="scss">
+    .nav-bar {
+
+    .el-tabs {
+
+    .el-tabs--border-card {
+        border: 0px solid #dcdfe6;
+        -webkit-box-shadow: 0 0px 0px 0;
+        box-shadow: 0 0px 0px 0;
+    }
+
+    .el-tabs__nav-scroll {
+        background-color: #2cb4ff;
+        padding-left: 60px;
+        padding-top: 6px;
+    }
+
+    .el-tabs__item.is-top.is-active {
+        color: #333333;
+    }
+
+    .el-tabs__item.is-top {
+        color: #ffffff;
+        border-radius: 6px 6px 0px 0px;
+    }
+
+    .el-tabs__item.is-top:hover {
+        color: #999999;
+    }
+
+    }
+
+    .divider {
+        display: inline-block;
+        margin: 0px 20px;
+        width: 1px;
+        height: 13px;
+        background-color: #d7d7d7;
+    }
+
+    }
+</style>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator'
+
+    @Component({
+        name: 'NavBar'
+    })
+    export default class extends Vue {
+        private activeName: string = 'first'
+
+        private handleClick(tab: any, event: any) {
+        }
+    }
+</script>
